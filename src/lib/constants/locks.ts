1c20b79c9d94bb5f993460d9d0c7a49175fd9a6a
TheOtherBrian1 <91111415+TheOtherBrian1@users.noreply.github.com>
2026-06-10 08:53:47 -0400

    Clarify lock descriptions in locks.ts
    Updated descriptions for ROW SHARE and ROW EXCLUSIVE locks to clarify their behavior regarding materialized views and foreign key constraints.

--- src/lib/constants/locks.ts
+++ src/lib/constants/locks.ts
@@ -9,13 +9,13 @@ const tableLocks = [
 		lock: 'ROW SHARE',
 		conflicts: ['EXCLUSIVE', 'ACCESS EXCLUSIVE'],
 		description:
-			'Functionally identical to ACCESS EXCLUSIVE (SELECT), but also prevents materialized views from refreshing concurrently.'
+			'Functionally identical to ACCESS SHARE (SELECT), but also prevents materialized views from refreshing concurrently.'
 	},
 	{
 		lock: 'ROW EXCLUSIVE',
 		conflicts: ['SHARE', 'SHARE ROW EXCLUSIVE', 'EXCLUSIVE', 'ACCESS EXCLUSIVE'],
 		description:
-			'Claimed by write operations and blocks indexes, materialized views, and triggers from being built. Without it, write operations could try writing to index files that are in a transitory state or inappropriately activate a partially created trigger. It also stops changes to table access rules (like RLS updates) or operations that overwrite the table file, protecting against data loss that could occur if the system tried changing data while the file was inaccessible.'
+			'Claimed by write operations and blocks indexes and triggers from being built. Also prevents foreign key constraints from being added. Without it, write operations could try writing to index files that are in a transitory state, disrupt the initial foreign key check, or inappropriately activate a partially created trigger. It also stops changes to table access rules (like RLS updates) or operations that overwrite the table file, protecting against data loss that could occur if the system tried changing data while the file was inaccessible.'
 	},
 	{
 		lock: 'SHARE UPDATE EXCLUSIVE',
