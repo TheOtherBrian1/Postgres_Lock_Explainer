c1c5eea133e45175d33a855018c3493d0347deef
Brian <briantgbtheonly@gmail.com>
2026-07-19 00:16:58 -0400

    added vercel support

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+	split: true
 };
-
-export default config;
